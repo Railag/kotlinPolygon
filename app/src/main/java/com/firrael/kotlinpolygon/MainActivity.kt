5fa19fe9ff1737c901917e598deef239cf77c7b6
Railag <whenthegroundcavedin@gmail.com>
2015-12-24 13:40:38 +0300

    anko setup

--- app/src/main/java/com/firrael/kotlinpolygon/MainActivity.kt
+++ app/src/main/java/com/firrael/kotlinpolygon/MainActivity.kt
@@ -2,13 +2,37 @@ package com.firrael.kotlinpolygon
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
+import android.support.v4.view.MarginLayoutParamsCompat
+import org.jetbrains.anko.*;
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        textView1.setText("test");
+
+        val name = "test1";
+
+        val mediumStyle = R.style.Base_TextAppearance_AppCompat_Medium_Inverse
+
+        verticalLayout {
+            padding = dip(20)
+            editText {
+                hint = "Name"
+                left = 10;
+                width = wrapContent
+            }
+            editText {
+                hint = "Password"
+                textSize = 24f
+                width = matchParent
+            }
+            button("Login") {
+                textSize = 26f
+            }.setTextAppearance(ctx, mediumStyle)
+
+            button("Test") {
+                onClick { ctx.toast("Hello, ${name}!") }
+            }.lparams(width = wrapContent)
+        }
     }
 }
